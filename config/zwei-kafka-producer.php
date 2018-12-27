24cbdd9ac84838fae46b846be408d46d83405226
赵威杰 <zhaoweijie@zhaoweijiedeMBP.lan>
2018-12-27 23:47:38 +0800

    caa: 1. 增加生产者kafka配置 2. 删除老版本配置

--- config/zwei-kafka-producer.php
+++ config/zwei-kafka-producer.php
@@ -6,6 +6,23 @@
  * Time: 11:37
  */
 return [
+    'v0_p_aliyun_common_user' => [// 生产者名(必须唯一): 阿里云kafka生产者
+        'cluster'   => 'normal',// 集群名: zwei-kafka-cluster.php中配置
+        'class'     => \Zwei\Kafka\Producer\SyncProducer::class,// 使用那种类型生产者发送消息: 同步生产者
+        'topics'    => [// 主题列表
+            "v0_t_aliyun_common",
+        ],
+        'options'   => [// kafka配置选项
+            "security.protocol"     => "sasl_ssl",
+            "sasl.mechanisms"       => "PLAIN",
+            "api.version.request"   => true,
+            "sasl.username"         => "user",
+            "sasl.password"         => "pass",
+            "ssl.ca.location"       => __DIR__.'/test/ca-cert',// 证书路径
+            "offset.store.method"   => "broker",// offset保存在broker中
+        ],
+    ],
+
     'v0_p_default_common_user_register' => [// 生产者名(必须唯一): 用户注册
         'cluster'   => 'default',// 集群名: zwei-kafka-cluster.php中配置
         'class'     => \Zwei\Kafka\Producer\AsyncProducer::class,// 使用那种类型生产者发送消息: 异步生产者
